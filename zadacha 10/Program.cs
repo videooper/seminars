bcd8223a7b5aa4c015007175660cb0ef496a9996
Andrey Ivanov <videooper@inbox.ru>
2022-08-24 11:44:03 +0300

    создали файл реадми

---
+++ zadacha 10/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+
+
+Console.Clear();
+Console.Write("Для выхода из программы команда Exit.\n или введите трёхзначное число: ");
+ while(true)
+    {
+        string input = Console.ReadLine();
+        if(!input.Equals("exit"))
+            Console.WriteLine("{0}->{1}",input, input[1]);
+            Console.WriteLine("Вторая цифра этого числа "+ (input[1]))
+       ;
+    }
