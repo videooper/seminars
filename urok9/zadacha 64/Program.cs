63afcd022e7e82f39de5e15ed787fd33c5afd50f
Andrey Ivanov <videooper@inbox.ru>
2022-09-01 22:18:35 +0300

    решили задачу 64

---
+++ urok9/zadacha 64/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в промежутке от M до N.
+
+//M = 1; N = 5. -> ""1, 2, 3, 4, 5""
+
+//M = 4; N = 8. -> ""4, 6, 7, 8""
+
+Console.Clear();
+Console.WriteLine($"Задача 64: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1.");
+int n = InputNumbers("Введите n: ");
+int count = 2;
+PrintNumber(n, count);
+Console.Write(1);
+
+void PrintNumber(int n, int count)
+{
+  if (count > n) return;
+  PrintNumber(n, count + 1);
+  Console.Write(count + ", ");
+}
+
+int InputNumbers(string input) 
+{
+  Console.Write(input);
+  int output = Convert.ToInt32(Console.ReadLine());
+  return output;
+}
